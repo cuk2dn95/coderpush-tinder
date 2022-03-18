3bbabbd74bf0808f34f725092802be22dd9fad5c
Khanh Truong Q <khanh.truongq@one-line.com>
2022-03-18 22:10:59 +0700

    Wrap Dio as HttpClient

---
+++ lib/data/remote/api.dart
@@ -0,0 +1,37 @@
+import 'package:coderpush_tinder/data/constant.dart';
+import 'package:dio/dio.dart';
+
+class HttpClient {
+  HttpClient() {
+    dio = Dio();
+    dio.options.baseUrl = AppConstant.baseUrl;
+    dio.interceptors.add(InterceptorsWrapper(
+        onRequest: (options, handler) {
+          options.headers.addEntries(
+              <String, dynamic>{'app-id': AppConstant.appId}.entries);
+          handler.next(options);
+        })
+    );
+  }
+
+  late Dio dio;
+
+  Future<Response<T>> get<T>(String path, {
+    Map<String, dynamic>? queryParameters,
+  }) => dio.get<T>(path, queryParameters: queryParameters);
+
+  Future<Response<T>> post<T>(String path, {
+    data,
+    Map<String, dynamic>? queryParameters,
+  }) => dio.post<T>(path, data: data, queryParameters: queryParameters);
+
+  Future<Response<T>> put<T>(String path, {
+    data,
+    Map<String, dynamic>? queryParameters,
+  }) => dio.put<T>(path, data: data, queryParameters: queryParameters);
+
+  Future<Response<T>> delete<T>(String path, {
+    data,
+    Map<String, dynamic>? queryParameters,
+  }) => dio.delete<T>(path, data: data, queryParameters: queryParameters);
+}
